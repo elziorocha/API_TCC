ae7e6e589dea1a6ae081b0a653ca865b600c5e2a
elziorocha <rochasenzo.04@gmail.com>
2025-05-10 14:52:20 -0300

    feat: validação de campos e correção de semântica no código de Documentos, trazendo tipagem.

--- src/controllers/AlunoDocumentoController.ts
+++ src/controllers/AlunoDocumentoController.ts
@@ -2,18 +2,25 @@ import { Request, Response } from "express";
 import { AlunoRepository } from "../repositories/AlunoRepository";
 import { AlunoDocumentoRepository } from "../repositories/AlunoDocumentoRepository";
 
+interface AlunoDocumento {
+  rg: string;
+  cpf: string;
+  orgao_emissor: string;
+  comprovante_matricula: string;
+  atestado_frequencia: string;
+  tipo_cartao: "EDUCARD" | "VEM";
+}
+
 export class AlunoDocumentoController {
   async create(req: Request, res: Response) {
-    const {
-      rg,
-      cpf,
-      orgao_emissor,
-      comprovante_matricula,
-      atestado_frequencia,
-      tipo_cartao,
-    } = req.body;
-
+    const alunoDocumentoData = req.body as AlunoDocumento;
     const { alunoId } = req.params;
+
+    if (!alunoDocumentoData.cpf || !alunoDocumentoData.rg) {
+      res.status(422).json({
+        message: "Campos de CPF e RG são obrigatórios.",
+      });
+    }
 
     try {
       const associarAoAluno = await AlunoRepository.findOneBy({
@@ -23,22 +30,21 @@ export class AlunoDocumentoController {
         res.status(404).json({ message: "Aluno não encontrado" });
         return;
       }
+      if (associarAoAluno.aluno_documento) {
+        res
+          .status(422)
+          .json({ message: "Aluno já possui documentos cadastrados." });
+        return;
+      }
 
-      const alunoDocumentoData = AlunoDocumentoRepository.create({
-        rg,
-        cpf,
-        orgao_emissor,
-        comprovante_matricula,
-        atestado_frequencia,
-        tipo_cartao,
-      });
+      const novoAlunoDocumento =
+        AlunoDocumentoRepository.create(alunoDocumentoData);
+      await AlunoDocumentoRepository.save(novoAlunoDocumento);
 
-      await AlunoDocumentoRepository.save(alunoDocumentoData);
-
-      associarAoAluno.aluno_documento = alunoDocumentoData;
+      associarAoAluno.aluno_documento = novoAlunoDocumento;
       await AlunoRepository.save(associarAoAluno);
 
-      res.status(201).json(alunoDocumentoData);
+      res.status(201).json(novoAlunoDocumento);
       return;
     } catch (error) {
       console.log(error);
