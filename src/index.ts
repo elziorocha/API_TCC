52dd99a70f2d824fac1463dba97d440d08aaca26
elziorocha <rochasenzo.04@gmail.com>
2025-10-17 20:11:09 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
 AppDataSource.initialize().then(() => {
   const app = express();
   app.use(
